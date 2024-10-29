a35a62b9530b2c50140a0cdd8240fd138f4d2a52
dannaward <daeunlee0720@gmail.com>
2024-10-29 16:25:10 +0900

    add match answers true when don't care test

--- src/test/kotlin/com/dannaward/junitlab/junitlab/model/ProfileTest.kt
+++ src/test/kotlin/com/dannaward/junitlab/junitlab/model/ProfileTest.kt
@@ -3,19 +3,28 @@ package com.dannaward.junitlab.junitlab.model
 import com.dannaward.junitlab.junitlab.model.criteria.Criteria
 import com.dannaward.junitlab.junitlab.model.criteria.Criterion
 import com.dannaward.junitlab.junitlab.model.question.BooleanQuestion
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.assertFalse
 
 class ProfileTest {
+    private lateinit var profile: Profile
+    private lateinit var question: BooleanQuestion
+    private lateinit var criteria: Criteria
+
+    @BeforeEach
+    fun create() {
+        profile = Profile("Bull Hockey, Inc.")
+        question = BooleanQuestion(1, "Got bonuses?")
+        criteria = Criteria()
+    }
+
     @Test
     fun matchAnswersFalseWhenMustMatchCriteriaNotMet() {
         // given
-        val question = BooleanQuestion(1, "Got bonuses?")
-
-        val profile = Profile("Bull Hockey, Inc.")
         val profileAnswer = Answer(question, Bool.FALSE)
 
-        val criteria = Criteria()
         val criteriaAnswer = Answer(question, Bool.TRUE)
         val criterion = Criterion(criteriaAnswer, Weight.MustMatch)
 
@@ -27,5 +36,23 @@ class ProfileTest {
 
         // then
         assertFalse { matches }
+    }
+
+    @Test
+    fun matchAnswersTrueForAnyDontCareCriteria() {
+        // given
+        val profileAnswer = Answer(question, Bool.FALSE)
+
+        val criteriaAnswer = Answer(question, Bool.TRUE)
+        val criterion = Criterion(criteriaAnswer, Weight.DontCare)
+
+        profile.add(profileAnswer)
+        criteria.add(criterion)
+
+        // when
+        val matches = profile.matches(criteria)
+
+        // then
+        assertTrue { matches }
     }
 }
